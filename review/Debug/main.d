03d283eddd5a05ced0c7ff26644fac179ee2bfc6
CommradeGeneral <m.hashim.666666@gmail.com>
2023-10-26 18:55:37 +0300

    first version

--- review/Debug/main.d
+++ review/Debug/main.d
@@ -5,27 +5,24 @@ main.d main.o: .././main.c .././generic_library/delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- .././MCAL/DIO/headers/DIO.h \
- e:\atmega\review\review\generic_library\std_data_types.h \
- e:\atmega\review\review\generic_library\bit_math.h \
  .././HAL/relay/headers/relay.h \
  e:\atmega\review\review\mcal\dio\headers\dio.h \
+ e:\atmega\review\review\generic_library\std_data_types.h \
+ e:\atmega\review\review\generic_library\bit_math.h \
  .././HAL/LCD_4_bit/headers/LCD_4_bit.h \
  e:\atmega\review\review\mcal\dio\headers\dio.h \
  .././HAL/LCD_8_bit/headers/LCD_8_bit.h \
  e:\atmega\review\review\mcal\dio\headers\dio.h \
- .././MCAL/GINT/headers/GINT.h \
- e:\atmega\review\review\generic_library\bit_math.h \
- e:\atmega\review\review\generic_library\std_data_types.h \
- .././MCAL/EXT_INT/headers/EXT_INT.h \
- e:\atmega\review\review\generic_library\bit_math.h \
- .././MCAL/EXT_INT/headers/EXT_INT_REG.h \
- e:\atmega\review\review\generic_library\std_data_types.h \
- e:\atmega\review\review\mcal\dio\headers\dio.h \
  .././HAL/rotary_encoder/headers/rotary_encoder.h \
  e:\atmega\review\review\mcal\dio\headers\dio.h \
  e:\atmega\review\review\mcal\ext_int\headers\ext_int.h \
+ e:\atmega\review\review\generic_library\bit_math.h \
+ e:\atmega\review\review\mcal\ext_int\headers\EXT_INT_REG.h \
+ e:\atmega\review\review\generic_library\std_data_types.h \
+ e:\atmega\review\review\mcal\dio\headers\dio.h \
  e:\atmega\review\review\mcal\gint\headers\gint.h \
+ e:\atmega\review\review\generic_library\bit_math.h \
+ e:\atmega\review\review\generic_library\std_data_types.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
@@ -45,15 +42,13 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
 
-.././MCAL/DIO/headers/DIO.h:
+.././HAL/relay/headers/relay.h:
+
+e:\atmega\review\review\mcal\dio\headers\dio.h:
 
 e:\atmega\review\review\generic_library\std_data_types.h:
 
 e:\atmega\review\review\generic_library\bit_math.h:
-
-.././HAL/relay/headers/relay.h:
-
-e:\atmega\review\review\mcal\dio\headers\dio.h:
 
 .././HAL/LCD_4_bit/headers/LCD_4_bit.h:
 
@@ -63,29 +58,25 @@ e:\atmega\review\review\mcal\dio\headers\dio.h:
 
 e:\atmega\review\review\mcal\dio\headers\dio.h:
 
-.././MCAL/GINT/headers/GINT.h:
-
-e:\atmega\review\review\generic_library\bit_math.h:
-
-e:\atmega\review\review\generic_library\std_data_types.h:
-
-.././MCAL/EXT_INT/headers/EXT_INT.h:
-
-e:\atmega\review\review\generic_library\bit_math.h:
-
-.././MCAL/EXT_INT/headers/EXT_INT_REG.h:
-
-e:\atmega\review\review\generic_library\std_data_types.h:
-
-e:\atmega\review\review\mcal\dio\headers\dio.h:
-
 .././HAL/rotary_encoder/headers/rotary_encoder.h:
 
 e:\atmega\review\review\mcal\dio\headers\dio.h:
 
 e:\atmega\review\review\mcal\ext_int\headers\ext_int.h:
 
+e:\atmega\review\review\generic_library\bit_math.h:
+
+e:\atmega\review\review\mcal\ext_int\headers\EXT_INT_REG.h:
+
+e:\atmega\review\review\generic_library\std_data_types.h:
+
+e:\atmega\review\review\mcal\dio\headers\dio.h:
+
 e:\atmega\review\review\mcal\gint\headers\gint.h:
+
+e:\atmega\review\review\generic_library\bit_math.h:
+
+e:\atmega\review\review\generic_library\std_data_types.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
 
